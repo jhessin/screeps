a8270a3d7c1fcb01f13828eb371b0a496532438d
Jim Hessin <jhessin@gmail.com>
2020-03-04 02:02:25 -0700

    General Updates

--- tutorial-1/main.js
+++ tutorial-1/main.js
@@ -1,3 +1,8 @@
-module.exports.loop = function () {
+const roleHarvester = require('role.harvester');
 
-}
+module.exports.loop = function () {
+  for(let name in Game.creeps){
+    const creep = Game.creeps[name];
+    roleHarvester.run(creep);
+  }
+};
