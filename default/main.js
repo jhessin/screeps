129434aaa89063c2e48319c9e724cecb8d4eda9c
Jim Hessin <jhessin@gmail.com>
2020-03-24 13:12:34 -0600

    Updated to fix bad formatting.
    Signed-off-by: Jim Hessin <jhessin@gmail.com>

--- default/main.js
+++ default/main.js
@@ -24,7 +24,7 @@ const MIN_REPAIRERS = 1;
 const MIN_WALL_REPAIRERS = 1;
 
 // The main entry point for the program
-module.exports.loop = function() {
+module.exports.loop = function () {
   console.log('----------------------');
   // check for memory entries of died creeps by iterating over Memory.creeps
   for (let name in Memory.creeps) {
@@ -69,25 +69,25 @@ module.exports.loop = function() {
   //  arrow function, which checks for the creep being a harvester
   let numberOfHarvesters = _.sum(
     Game.creeps,
-    crp => crp.memory.role === c.HARVESTER? 1 : 0
+    crp => crp.memory.role === c.HARVESTER ? 1 : 0
   );
   let numberOfUpgraders = _.sum(
     Game.creeps,
-    crp => crp.memory.role === c.UPGRADER? 1 : 0
+    crp => crp.memory.role === c.UPGRADER ? 1 : 0
   );
   let numberOfBuilders = _.sum(
     Game.creeps,
-    crp => crp.memory.role === c.BUILDER? 1 : 0
+    crp => crp.memory.role === c.BUILDER ? 1 : 0
   );
   let numberOfRepairers = _.sum(
     Game.creeps,
-    crp => crp.memory.role === c.REPAIRER? 1 : 0
+    crp => crp.memory.role === c.REPAIRER ? 1 : 0
   );
   let numberOfWallRepairers = _.sum(
     Game.creeps,
-    crp => crp.memory.role === c.WALL_REPAIRER? 1 : 0
+    crp => crp.memory.role === c.WALL_REPAIRER ? 1 : 0
   );
-  let numberOfBuckets = _.sum(Game.creeps, crp => crp.name.startsWith('BB')? 1 : 0);
+  let numberOfBuckets = _.sum(Game.creeps, crp => crp.name.startsWith('BB') ? 1 : 0);
 
   // console.log(`Harvesters: ${numberOfHarvesters}`);
   // console.log(`Upgraders: ${numberOfUpgraders}`);
@@ -112,42 +112,23 @@ module.exports.loop = function() {
       roleTower.run(tower);
     }
 
-    // if not enough harvesters
+    // Spawn all of our creeps
+    // Harvesters
     if (numberOfHarvesters < MIN_HARVESTERS) {
-      // try to spawn one
-      // name = roleHarvester.build(spawn);
-      name = spawn.createCreep(energy, c.HARVESTER);
+      name = spawn.spawnCustom(energy, c.HARVESTER);
 
-      if (name === ERR_NOT_ENOUGH_ENERGY && numberOfHarvesters === 0) {
-        name = spawn.createCreep(spawn.room.energyAvailable, c.HARVESTER);
-      }
-    }
-    // if not enough upgraders
-    else if (numberOfUpgraders < MIN_UPGRADERS) {
-      // try to spawn one
-      name = spawn.createCreep(energy, c.UPGRADER);
-    }
-    // if not enough repairers
-    else if (numberOfRepairers < MIN_REPAIRERS) {
-      // try to spawn one
-      name = spawn.createCreep(energy, c.REPAIRER);
-    }
-    // if not enough builders
-    else if (numberOfBuilders < MIN_BUILDERS) {
-      // try to spawn one
-      name = spawn.createCreep(energy, c.BUILDER);
-    } else if (numberOfWallRepairers < MIN_WALL_REPAIRERS) {
-      name = spawn.createCreep(energy, c.WALL_REPAIRER);
-    } else if (numberOfBuckets < c.NUM_BUCKETS) {
-      name = spawn.createCreep(energy, c.BUCKETIER);
-    } else {
-      // DEFAULT to Upgraders
-      name = spawn.createCreep(energy, c.UPGRADER);
-    }
+      if (name === ERR_NOT_ENOUGH_ENERGY && numberOfHarvesters === 0) name = spawn.spawnCustom(spawn.room.energyAvailable, c.HARVESTER);
+    } else if (numberOfUpgraders < MIN_UPGRADERS) name = spawn.spawnCustom(energy, c.UPGRADER);
+    else if (numberOfRepairers < MIN_REPAIRERS) name = spawn.spawnCustom(energy, c.REPAIRER);
+    else if (numberOfBuilders < MIN_BUILDERS) name = spawn.spawnCustom(energy, c.BUILDER);
+    else if (numberOfWallRepairers < MIN_WALL_REPAIRERS) name = spawn.spawnCustom(energy, c.WALL_REPAIRER);
+    else if (numberOfBuckets < c.NUM_BUCKETS) name = spawn.spawnCustom(energy, c.BUCKETIER);
+    // DEFAULT to Upgraders
+    else name = spawn.spawnCustom(energy, c.UPGRADER);
 
     // print name to console if spawning was a success
     // name > 0 would not work since string > 0 returns false
-    if (name && !(name < 0)) {
+    if (typeof name === 'string') {
       let creep = Game.creeps[name];
       console.log('Spawned new ' + creep.memory.role + ' creep: ' + name);
     }
--- default/prototype.spawn.js
+++ default/prototype.spawn.js
@@ -28,35 +28,35 @@ const NAMES = [
   'Nehemiah',
   'Simian',
 ];
-module.exports = function() {
-  StructureSpawn.prototype.createCreep =
-		function(energy, role) {
-		  let i;
-		  let numberOfParts = Math.floor(energy / 200);
-		  let body = [];
+module.exports = function () {
+  StructureSpawn.prototype.spawnCustom =
+    function (energy, role) {
+      let i;
+      let numberOfParts = Math.floor(energy / 200);
+      let body = [];
 
-		  for (i = 0; i < numberOfParts; i++) {
-		    body.push(WORK);
-		  }
-		  for (i = 0; i < numberOfParts; i++) {
-		    body.push(CARRY);
-		  }
-		  for (i = 0; i < numberOfParts; i++) {
-		    body.push(MOVE);
-		  }
+      for (i = 0; i < numberOfParts; i++) {
+        body.push(WORK);
+      }
+      for (i = 0; i < numberOfParts; i++) {
+        body.push(CARRY);
+      }
+      for (i = 0; i < numberOfParts; i++) {
+        body.push(MOVE);
+      }
 
-		  if (role === c.BUCKETIER) {
-		    for (i = 1; i < c.NUM_BUCKETS; i++) {
-		      if (!Game.creeps[`BB${i}`]) {
-		        this.spawnCreep(body, `BB${i}`, { working: false });
-		      }
-		    }
-		  } else {
-        for (name of NAMES) {
-          let r = this.spawnCreep(body, name, { role, working: false });
+      if (role === c.BUCKETIER) {
+        for (i = 1; i < c.NUM_BUCKETS; i++) {
+          if (!Game.creeps[`BB${i}`]) {
+            this.spawnCreep(body, `BB${i}`, {working: false});
+          }
+        }
+      } else {
+        for (let name of NAMES) {
+          let r = this.spawnCreep(body, name, {role, working: false});
           if (r === ERR_NAME_EXISTS) continue;
           return r;
         }
-		  }
-		};
+      }
+    };
 };
