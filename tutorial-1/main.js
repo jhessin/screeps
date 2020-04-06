2157977f0ae4d7eacbfbbaab4c653cf5b7c11535
Jim Hessin <jhessin@gmail.com>
2020-04-06 17:07:09 -0600

    Setup for Atom

--- tutorial-1/main.js
+++
@@ -1,8 +0,0 @@
-const roleHarvester = require('role.harvester');
-
-module.exports.loop = function () {
-  for(let name in Game.creeps){
-    const creep = Game.creeps[name];
-    roleHarvester.run(creep);
-  }
-};
