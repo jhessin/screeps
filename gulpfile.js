e7e5a711f06cadd45354115b254bd89d7007bf19
Jim Hessin <jhessin@gmail.com>
2020-04-07 02:49:07 -0500

    removed unused config files

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const screeps = require('gulp-screeps');
-
-gulp.task('screeps', () => {
-  gulp.src('./dist/main.js')
-  .pipe(screeps(options))
-})
-
--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // The main entry point for the program
 export function loop() {
-  console.log(`Current game tick: ${Game.time}`);
-
-  if (Game.time % 7 === 0) {
+  // console.log(`Current game tick: ${Game.time}`);
+  console.log('=================================');
+  console.log(`cpu.limit: ${Game.cpu.limit}`);
+  console.log(`cpu.tickLimit: ${Game.cpu.tickLimit}`);
+  console.log(`cpu.limit: ${Game.cpu.limit}`);
+  if (Game.time % 500 === 0) {
     cleanupMemory();
   }
 }
 
 function cleanupMemory() {
   // check for memory entries of died creeps by iterating over Memory.creeps
+  console.log('Cleaning up dead creeps.');
   for (let name in Memory.creeps) {
     // and checking if the creep is still alive
     if (!Game.creeps[name]) {
